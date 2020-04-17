4084d80314a5004136b65cfb258f09f0d23b67d8
gustavo <gustavo.oliveira@atuo.in>
2020-04-17 19:59:59 -0300

    adicionado hello world

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function Login(){
     return (
-        <View />
+        <View style={styles.wrapContent}>
+            <Text style={styles.text}>Alisson funcionou!</Text>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    wrapContent: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+})
 
 export default Login;
