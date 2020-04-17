55e451620ea5d787e4d3ee50720a4163a9eecaff
gustavo <gustavo.oliveira@atuo.in>
2020-04-17 20:40:54 -0300

    adicionado trangulos do layout

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 function Login(){
     return (
         <View style={styles.wrapContent}>
-            <Text style={styles.text}>Hellow World!</Text>
+            <View style={styles.triangleOne}/>
+            <View style={styles.triangleTwo}/>
+            <View style={styles.triangleThree}/>
         </View>
     );
 }
@@ -12,10 +14,43 @@ function Login(){
 const styles = StyleSheet.create({
     wrapContent: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        position: 'relative',
+        backgroundColor: '#303036'
     },
+    triangleOne: {
+        position: 'absolute',
+        top: 20,
+        right: -60,
+        width: 120,
+        height: 120,
+        backgroundColor: '#FD3539',
+        transform: [
+            {rotate: '45deg'}
+        ]
+    },
+    triangleTwo: {
+        position: 'absolute',
+        top: 300,
+        left: -60,
+        width: 120,
+        height: 120,
+        backgroundColor: '#FD3539',
+        transform: [
+            {rotate: '45deg'}
+        ]
 
+    },
+    triangleThree: {
+        position: 'absolute',
+        bottom: -100,
+        right: -150,
+        width: 200,
+        height: 200,
+        backgroundColor: '#FD3539',
+        transform: [
+            {rotate: '130deg'}
+        ]
+    },
     text: {
         fontSize: 30,
         fontWeight: 'bold'
