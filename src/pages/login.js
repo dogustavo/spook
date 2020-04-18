a8f843955040e3b6fc031a47477a5c4539c15f36
gustavo <gustavo.oliveira@atuo.in>
2020-04-17 21:32:34 -0300

    iniciado layout da tela de login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,67 +1,30 @@
 import React from 'react';
 import { 
     View, 
-    StyleSheet, 
-    Image 
+    Image,
+    TextInput 
 } from 'react-native';
 
 import logo from '../assets/images/Logo.png';
+import loginStyles from './loginStyles'
 
 function Login(){
     return (
-        <View style={styles.wrapContent}>
-            <View style={styles.triangleOne}/>
-            <View style={styles.triangleTwo}/>
-            <View style={styles.triangleThree}/>
-            <Image source={ logo } style={styles.logoImage}/>
+        <View style={loginStyles.wrapContent}>
+            <View style={loginStyles.triangleOne}/>
+            <View style={loginStyles.triangleTwo}/>
+            <View style={loginStyles.triangleThree}/>
+            <Image source={ logo } style={loginStyles.logoImage}/>
+
+            <TextInput 
+                style={loginStyles.input}
+                placeholder='E-MAIL'
+                placeholderTextColor='#FFF'
+            />  
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    wrapContent: {
-        flex: 1,
-        position: 'relative',
-        backgroundColor: '#303036'
-    },
-    triangleOne: {
-        position: 'absolute',
-        top: 20,
-        right: -60,
-        width: 120,
-        height: 120,
-        backgroundColor: '#FD3539',
-        transform: [
-            {rotate: '45deg'}
-        ]
-    },
-    triangleTwo: {
-        position: 'absolute',
-        top: 300,
-        left: -60,
-        width: 120,
-        height: 120,
-        backgroundColor: '#FD3539',
-        transform: [
-            {rotate: '45deg'}
-        ]
-    },
-    triangleThree: {
-        position: 'absolute',
-        bottom: -100,
-        right: -150,
-        width: 200,
-        height: 200,
-        backgroundColor: '#FD3539',
-        transform: [
-            {rotate: '130deg'}
-        ]
-    },
-    logoImage: {
-        width: 190,
-        alignSelf: 'center',
-        top: 110
-    }
-})
+
 
 export default Login;
