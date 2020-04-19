fb622d5d2a988041ccb6602d74592098c7112f16
WandersonMJ <wanderson.marques@fatecitapetininga.edu.br>
2020-04-18 21:56:46 -0300

    Selecionar Imagem

--- src/pages/createUser/createStepOne.js
+++ src/pages/createUser/createStepOne.js
@@ -1,26 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-native-modal';
 import { 
     View, 
     Text,
     TouchableOpacity,
-    Image
+    Image,
+    PushNotificationIOS,
+    Button
 } from 'react-native';
 
 import createUser from '../../styles/createUser';
 import stepProgress from '../../styles/stepProgress';
 import camera from '../../assets/images/take-picture.png';
 import storageUser from "../store";
+import ImagePicker from "react-native-image-picker";
+
 
 
 export default function userStepOne({ navigation }) {
+    const [image, setImage] = useState(null);
 
     function navigateToLogin() {
         navigation.navigate('Login');
     }
 
-    function SaveImg(){
-        storageUser()
+    const SelectImage = async () =>{
+        const options = {
+            title: 'Selecione uma Foto',
+            customButtons: [{ name: 'fb', title: 'Escolha uma foto da Galeria'}],
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+          };
+
+        ImagePicker.launchImageLibrary(options, response => {
+            console.log('Response = ', response);
+            
+            if(response.didCancel){
+                console.log("Usuário fechou o selecionador de imagem");
+            }else if (response.error){
+              console.log(response.error);
+            }else if (response.customButton) {
+              console.log('Selected: ', response.customButton)
+            } else {
+              const source = {uri: response.uri };
+          
+              setImage(source)
+            }
+          });
     }
 
 
@@ -43,9 +71,22 @@ export default function userStepOne({ navigation }) {
             >
                 <View style={createUser.modalContent}>
                     <Text style={createUser.modalTitle}>Escolha uma bela foto de peril</Text>
-                    <TouchableOpacity style={createUser.cameraOpen} >
-                        <Image source={ camera } style={createUser.cameraImage}/>
+                    <TouchableOpacity onPress={SelectImage} style={createUser.cameraOpen} >
+                        {image ? 
+                        <Image 
+                            source={image}
+                            style={{width: 300, height: 300}}
+                        />
+                       
+                        : 
+                        <Image 
+                            source={ camera } 
+                            style={createUser.cameraImage}
+                        />
+                        }
                     </TouchableOpacity>
+
+                       
 
                     <View style={createUser.modalFooter}>
                         <TouchableOpacity style={createUser.buttonCancel} >
